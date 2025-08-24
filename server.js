f7a39392c70124ce9b410fee0b26ffc6f33b2240
Anamd-hub <anasmdl120@gmail.com>
2025-08-25 01:27:39 +0300

    chore: update server.js to use routes and middleware

--- server.js
+++ server.js
@@ -1,75 +1,23 @@
 const express = require("express");
+const logger = require("./middleware/logger");
+const notesRoutes = require("./routes/notes");
+
 const app = express();
 const PORT = 5000;
 
-// In-memory notes storage
-let notes = [
-  { id: 1, title: "Learn Node.js", content: "Study Express basics" },
-  { id: 2, title: "Practice React", content: "Build components" },
-];
-
-// Middleware to parse JSON
+// Middleware
 app.use(express.json());
+app.use(logger);
+
+// Routes
+app.use("/notes", notesRoutes);
 
 // Test route
 app.get("/", (req, res) => {
   res.send("Notes API is running...");
 });
 
-app.get("/notes", (req, res) => {
-  res.json(notes);
-})
-
-app.get("/notes/:id", (req, res) => {
-  const noteId = parseInt(req.params.id);
-  const note = notes.find(n => n.id === noteId);
-  if (note) {
-    res.json(note);
-  } else {
-    res.status(404).json({ message: "Note not found" });
-  }
-})
-
-app.post("/notes", (req, res) => {
-  const { title, content } = req.body;
-  if (!title || !content) {
-    return res.status(400).json({ message: "Title and content are required" });
-  }
-
-  const newNote = {
-    id: notes.length + 1,
-    title,
-    content,
-  }
-
-  notes.push(newNote);
-  res.status(201).json({
-    message: "Note created",
-    note: newNote,
-  });
-})
-
-app.put("/notes/:id", (req, res) => {
-  const note = notes.find(n => n.id === parseInt(req.params.id));
-  if (!note) {
-    return res.status(404).json({ message: "Note not found" });
-  }
-
-  const { title, content } = req.body;
-  note.title = title || note.title;
-  note.content = content || note.content;
-
-  res.json({
-    message: "Note updated",
-    note,
-  });
-})
-
-app.delete("/notes/:id", (req, res) => {
-  notes.filter(n => n.id !== parseInt(req.params.id));
-  res.json({ message: "Note deleted" });
-})
-
+// Start server
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
